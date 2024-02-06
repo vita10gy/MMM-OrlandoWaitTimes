ebeebd408a06733dc73eab304d9c02e789088137
vita10gy <vita10gy@charter.net>
2024-02-06 17:08:58 -0500

    Name error

--- examples/UniversalStudios/UniversalStudiosFlorida.js
+++ examples/UniversalStudios/UniversalStudiosFlorida.js
@@ -1,6 +1,6 @@
 const exampleModule = {
   module: "MMM-ThemeParkWaitTimes",
-  header: "Hollywood Studios", //You can call this whatever
+  header: "Universal Studios", //You can call this whatever
   position: "top_left",
   config: {
     futureHours: false, //allows you to hide the hours for upcoming days
